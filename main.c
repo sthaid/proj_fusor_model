9e354b8a3d2028cbaedef888db99ca08abbd99bb
Steven Haid <stevenhaid@gmail.com>
2017-10-12 08:22:30 -0400

    start coding the display handler

--- main.c
+++ main.c
@@ -95,11 +95,128 @@ static void help(void)
 
 // -----------------  DISPLAY_HANDLER  --------------------------------------------------
 
+//#define DEFAULT_WIN_WIDTH    1920
+//#define DEFAULT_WIN_HEIGHT   1000
+#define DEFAULT_WIN_WIDTH    1900
+#define DEFAULT_WIN_HEIGHT   1000
+
+#define FUSOR_PANE_SIZE       800
+
+// XXX
+char about[] = "\
+hello\n\
+123";
+static void draw_fusor_pane(rect_t * border_pane, rect_t * pane);
+
 static void display_handler(void)
 {
-    INFO("starting\n");
-    model_start();
-    pause();
-    INFO("done\n");
+    //INFO("starting\n");
+    //model_start();
+    //pause();
+    //INFO("done\n");
 
+    int32_t       win_width, win_height;
+    rect_t        fusor_pane_full, fusor_pane;
+    bool          redraw, done=false;
+    sdl_event_t * event;
+
+    // XXX maybe sdl_init shoud draw the screen and process any events,and
+    //     return the actual size
+    win_width  = DEFAULT_WIN_WIDTH;
+    win_height = DEFAULT_WIN_HEIGHT;
+    if (sdl_init(&win_width, &win_height, true, true, NULL) < 0) {
+        FATAL("sdl_init %dx%d failed\n", win_width, win_height);
+    }
+
+    DEBUG("XXX W H  %d %d\n", win_width, win_height);
+
+    sdl_init_pane(&fusor_pane_full, &fusor_pane,
+                  0, 0,
+                  FUSOR_PANE_SIZE, FUSOR_PANE_SIZE);
+
+    // loop until done
+    while (!done) {
+        // XXX should the panes be inited after this
+        sdl_get_state(&win_width, &win_height, NULL);
+        DEBUG("win_width = %d win_height = %d\n", win_width, win_height);
+
+        // initialize for display update
+        sdl_display_init();
+
+        // draw panes
+        draw_fusor_pane(&fusor_pane_full, &fusor_pane);
+
+        // register for events   
+        sdl_event_register(SDL_EVENT_KEY_SHIFT_ESC, SDL_EVENT_TYPE_KEY, NULL);   // done (shift-esc key)
+        sdl_event_register('?', SDL_EVENT_TYPE_KEY, NULL);
+        sdl_event_register('r', SDL_EVENT_TYPE_KEY, NULL);
+        sdl_event_register('q', SDL_EVENT_TYPE_KEY, NULL);
+
+        // present the display
+        sdl_display_present();
+
+        // process events until either redraw or done flag is set
+        redraw = false;
+        while (true) {
+            event = sdl_poll_event();
+            if (event->event != SDL_EVENT_NONE) {
+                DEBUG("XXX GOT EVENT %d\n", event->event);
+            }
+            switch (event->event) {
+            case SDL_EVENT_QUIT: case SDL_EVENT_KEY_SHIFT_ESC: case 'q':
+                sdl_play_event_sound();
+                done = true;
+                break;
+            case '?':
+                sdl_play_event_sound();
+                sdl_display_text(about);
+                redraw = true;
+                break;
+            case 'r':
+                sdl_play_event_sound();
+                redraw = true;
+                break;
+            case SDL_EVENT_SCREENSHOT_TAKEN:
+                sdl_play_event_sound();
+                redraw = true;
+                break;
+            case SDL_EVENT_WIN_SIZE_CHANGE:
+            case SDL_EVENT_WIN_RESTORED:
+                redraw = true;
+                break;
+            default:
+                break;
+            }
+
+            if (done) {
+                return;
+            }
+            if (redraw) {
+                break;
+            }
+            usleep(1000);
+        }
+    }
 }
+
+static void draw_fusor_pane(rect_t * border_pane, rect_t * pane)
+{
+    // draw the border
+    DEBUG("%d %d %d %d\n",
+        border_pane->x,
+        border_pane->y,
+        border_pane->w,
+        border_pane->h);
+    sdl_render_pane_border(border_pane, GREEN);
+
+    // draw circles to represent the chamber and the grid
+    sdl_render_text(pane, 0, 0, 0, "LIVE", GREEN, BLACK);
+
+    // draw the particles, use red for ion and green for atom;
+    // use larger size when zoomed in
+}
+
+//draw_circle(rect_t * pane) 
+//{
+    //// on first call create table of sin values
+//}
